23b7aa0b97cafbebbb21a7a12ce4291c6317e0d3
shawn <echun1234@gmail.com>
2020-11-23 19:52:13 +0900

    modify user info section

--- exex/lib/ui/about_page.dart
+++ exex/lib/ui/about_page.dart
@@ -1,3 +1,4 @@
+import 'package:exex/Widgets/navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
@@ -15,43 +16,121 @@ class AboutPage extends StatelessWidget {
 
     return Scaffold(
 
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 50.0,
-            ),
-            Text(
-              'About Page',
-              style: TextStyle(fontSize: 30),
-            ),
-            SizedBox(
-              height: 50.0,
-            ),
-            Stack(
-              children: <Widget>[
-                SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage('https://picsum.photos/250?image=9'),
-                    backgroundColor: Color(0xFFFFFFFF),
-                  ),
-
-                ),
-                Text('flunge')
-
-
-              ],
-            )
-
-
-          ],
-        )
+      body: ListView(
+        children: [
+          userInfoSection,
+          const Divider(
+            color: Colors.black38,
+            height: 30,
+            thickness: 2,
+            indent: 100,
+            endIndent: 100,
+          ),
+          userInterestSection
+        ],
       )
+
+
     );
 
   }
+
+
+  Widget userInfoSection = Container(
+    //color: Colors.pink,
+    padding: const EdgeInsets.all(100),
+    
+    child: Row(
+      //crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 150,
+          height: 150,
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://picsum.photos/250?image=9'),
+            backgroundColor: Color(0xFFFFFFFF),
+          ),
+        ),
+        Padding(padding: EdgeInsets.all(30),),
+        Column(
+          // mainAxisSize: MainAxisSize.min,
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            
+            Text(
+              'flunge',
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(
+              child: Text('spring framework 기말 과제 \n flutter로 작업중'),
+              //width: 100,
+              //height: 100,
+            ),
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(30),),
+        Column(
+          children: <Widget>[
+            Text('팔로우',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text('0', 
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(20),),
+        Column(
+          children: <Widget>[
+            Text('팔로워',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text('0', 
+              style: TextStyle(fontWeight: FontWeight.bold),
+            )
+          ],
+        )
+        
+      ],
+    ),
+  );
+
+  Widget userInterestSection = Container(
+
+    child : Column(
+      children: <Widget>[
+        //NavigationBar()
+      ],
+    )
+
+    // child: Row(
+    //   children: [
+    //     Padding(padding: EdgeInsets.all(30),),
+    //     Column(
+    //       children: <Widget>[
+    //         Text(
+    //           'fafafafafaf',
+    //           style: TextStyle(fontSize: 40),
+    //         ),
+    //         Text(
+    //           'fafafafafaf',
+    //           style: TextStyle(fontSize: 40),
+    //         ),
+    //         Text(
+    //           'fafafafafaf\n\n\n\n\n\n\n\n\n\n\naffaf',
+    //           style: TextStyle(fontSize: 40),
+    //         ),
+    //       ],
+    //     )
+        
+    //   ],
+    // ),
+  );
+
+
+
 }
 
 
